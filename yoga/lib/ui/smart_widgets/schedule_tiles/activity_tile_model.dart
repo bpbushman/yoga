be8462ca1bfdfb95667d78d6b30e6c4574c39bdb
bpbushman <50936188+bpbushman@users.noreply.github.com>
2020-08-06 20:23:58 -0600

    added snack bar with icon taps

--- yoga/lib/ui/smart_widgets/schedule_tiles/activity_tile_model.dart
+++ yoga/lib/ui/smart_widgets/schedule_tiles/activity_tile_model.dart
@@ -1,6 +1,12 @@
 import 'package:stacked/stacked.dart';
+//import 'package:injectable/injectable.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:yoga/app/locator.dart';
 
+
+//@lazySingleton
 class ActivityTileModel extends BaseViewModel {
+  final SnackbarService _snackbarService = locator<SnackbarService>();
   bool isLiked = false;
   bool alarm = false;
   bool isGoing = false;
@@ -8,15 +14,30 @@ class ActivityTileModel extends BaseViewModel {
   void toggleGoing() {
     isGoing = !isGoing;
     notifyListeners();
+    if(isGoing) {
+      _snackbarService.showSnackbar(
+        message: 'going!', 
+        duration: Duration(seconds: 3));
+    }
   }
 
   void toggleAlarm() {
     alarm = !alarm;
     notifyListeners();
+    if(alarm) {
+      _snackbarService.showSnackbar(
+        message: 'reminder set!', 
+        duration: Duration(seconds: 3));
+    }
   }
 
   void toggleLike() {
     isLiked = !isLiked;
     notifyListeners();
+    if(isLiked) {
+      _snackbarService.showSnackbar(
+        message: 'added to favorites!', 
+        duration: Duration(seconds: 3));
+    }
   }
 }
